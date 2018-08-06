9b1766fc888b8a777f377f5537680c330fc9ba06
wing-yiu <wing-yiu@users.noreply.github.com>
2018-08-06 14:37:59 +0800

    able to upload images and save to local

--- flaskapp/__init__.py
+++ flaskapp/__init__.py
@@ -1,5 +1,7 @@
+import os
 from flask import Flask
 
 app = Flask(__name__)
+app.config['UPLOADED_PHOTOS_DEST'] = os.getcwd() + '/flaskapp/static'
 
 from flaskapp import routes
--- flaskapp/routes.py
+++ flaskapp/routes.py
@@ -1,6 +1,25 @@
+import os
+
+from flask import Flask, render_template, request
+from werkzeug import secure_filename
+
 from flaskapp import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'username': 'Wing Yiu'}
+    return render_template('index.html', title='Home', user=user)
+
+
+@app.route('/upload')
+def upload():
+   return render_template('upload.html')
+
+@app.route('/ocr', methods=['GET', 'POST'])
+def upload_file():
+   if request.method == 'POST':
+      f = request.files['file']
+      filename = secure_filename(f.filename)
+      f.save(os.path.join(app.config['UPLOADED_PHOTOS_DEST'], filename))
+      return 'file uploaded successfully'
