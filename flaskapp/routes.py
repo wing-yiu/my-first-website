f2473d634271c9f9f7e2a7d1da84a586c27b5baf
wing-yiu <wing-yiu@users.noreply.github.com>
2018-08-07 12:24:41 +0800

    Post request to google API instead of using python client

--- flaskapp/routes.py
+++ flaskapp/routes.py
@@ -20,7 +20,7 @@ def upload():
     return render_template('upload.html')
 
 
-# perform ocr on the file and render, but DO NOT SAVE
+# perform ocr on the file using python client
 @app.route('/ocr', methods=['GET', 'POST'])
 def ocr_file():
     if request.method == 'POST':
@@ -32,6 +32,32 @@ def ocr_file():
 
         # perform ocr
         ocr_text = detect_text(filepath)
+
+    # render ocr results
+    return render_template('ocr_result.html',
+                           ocr_image=filename,
+                           ocr_results=ocr_text)
+
+
+# perform ocr using api
+from flaskapp.vision_api import (image_converter,
+                                 make_request,
+                                 format_response)
+
+
+@app.route('/ocr2', methods=['GET', 'POST'])
+def ocr_text():
+    if request.method == 'POST':
+        f = request.files['file']
+        # save file to local
+        filename = secure_filename(f.filename)
+        filepath = os.path.join(app.config['UPLOADED_PHOTOS_DEST'], filename)
+        f.save(filepath)
+
+        # perform ocr
+        image_64 = image_converter(filepath)
+        response = make_request(image_64)
+        ocr_text = format_response(response.text)
 
     # render ocr results
     return render_template('ocr_result.html',
