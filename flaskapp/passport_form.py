11cf74957648189e62ab8348afb025384e7dcabc
wing-yiu <wing-yiu@users.noreply.github.com>
2018-08-08 11:07:30 +0800

    Add more fields to the form

--- flaskapp/passport_form.py
+++ flaskapp/passport_form.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import (StringField, IntegerField, TextAreaField,
+from wtforms import (StringField, DateField, TextAreaField,
                      SubmitField, RadioField, SelectField)
 from flask_wtf.file import FileField, FileAllowed, FileRequired
 from wtforms.validators import DataRequired
@@ -7,8 +7,13 @@ from wtforms.validators import DataRequired
 
 class PassportForm(FlaskForm):
     name = StringField('Name', validators=[DataRequired()])
+    surname = StringField('Surname', validators=[DataRequired()])
     gender = RadioField('Gender', choices=[('M', 'Male'), ('F', 'Female')])
-    file = FileField('Passport', validators=[FileRequired(),
+    birth_date = DateField('Date of Birth', format='%d/%m/%Y', validators=[DataRequired()])
+    passport_num = StringField('Passport Number', validators=[DataRequired()])
+    nationality = StringField('Nationality', validators=[DataRequired()])
+    expiration_date = DateField('Expiration Date', format='%d/%m/%Y', validators=[DataRequired()])
+    file = FileField('Passport Image', validators=[FileRequired(),
                                                FileAllowed(['jpg', 'png'],
                                                            '*jpg and *png images only!')
                                                ])
--- flaskapp/routes.py
+++ flaskapp/routes.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, flash
 from werkzeug import secure_filename
 
 from flaskapp import app
@@ -21,20 +21,25 @@ def index():
 @app.route('/upload', methods=['GET', 'POST'])
 def upload():
     form = PassportForm()
-    if form.validate_on_submit():
-        file = request.files['file']
+    if request.method == 'POST':
+        if form.validate_on_submit():
+            file = request.files['file']
 
-        filename = secure_filename(file.filename)
-        filepath = os.path.join(app.config['UPLOADED_PHOTOS_DEST'], filename)
-        file.save(filepath)
+            filename = secure_filename(file.filename)
+            filepath = os.path.join(app.config['UPLOADED_PHOTOS_DEST'], filename)
+            file.save(filepath)
 
-        # perform ocr
-        image_64 = image_converter(filepath)
-        response = make_request(image_64)
-        ocr_text = format_response(response.text)
-        return render_template('ocr_result.html',
-                               ocr_image=filename,
-                               ocr_results=ocr_text)
+            # perform ocr
+            image_64 = image_converter(filepath)
+            response = make_request(image_64)
+            ocr_text = format_response(response.text)
+            return render_template('ocr_result.html',
+                                   ocr_image=filename,
+                                   ocr_results=ocr_text)
+
+        else:
+            flash('ERROR')
+            return render_template('upload.html', form=form)
     return render_template('upload.html', form=form)
 
 
